8020762e678b5d6025c322a126dcc0fcbde61d7a
liasica <magicrolan@qq.com>
2016-08-04 10:01:08 +0800

    开发提交

---
+++ SmsSdk.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author: liasica
+ * Email: magicrolan@qq.com
+ * CreateTime: 16/7/31 下午7:20
+ */
+namespace liasica\chinasms;
+
+class SmsSdk
+{
+    /**
+     * 发送
+     */
+    public function send()
+    {
+
+    }
+}
---
+++ suppliers/Huaxin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Author: liasica
+ * Email: magicrolan@qq.com
+ * CreateTime: 16/8/4 上午9:53
+ */
+namespace liasica\chinasms\suppliers;
+
+class Huaxin
+{
+}
